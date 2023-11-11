9e01e88fd942919b52be7951d785924f92605885
Rammah <rammahkarpous@outlook.com>
2023-11-11 13:22:40 +0100

    feat: create navigation bar

---
+++ components/Navigation.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import React from 'react'
+
+import { HiBugAnt } from 'react-icons/hi2'
+
+const Navigation = () => {
+
+    const links = [
+        { name: 'Dashboard', href: '/' },
+        { name: 'Issues', href: '/issues' }
+    ]
+
+    return (
+        <nav className='flex items-center justify-between container m-auto border-b border-gray-200 mb-5'>
+            <Link href="/"><HiBugAnt /></Link>
+
+            <ul className='flex'>
+                { links.map( link => 
+                    <li key={link.href}>
+                        <Link 
+                            className='p-4 text-zinc-500 hover:text-zinc-800 inline-block transition-colors' 
+                            href={link.href}>{link.name}
+                        </Link>
+                    </li> 
+                ) }
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation
